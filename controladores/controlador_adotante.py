b55871aaca262c0d46aeebe9c42cb55b08dbb1b5
Bridge-PC <giovanespace@gmail.com>
2023-06-15 17:04:13 -0300

    Tela adotante completa

--- controladores/controlador_adotante.py
+++ controladores/controlador_adotante.py
@@ -105,9 +105,17 @@ class ControladorAdotante:
             self.__tela_adotante.mensagem("Adotante inexistente no sistema.")
 
     def listar_adotantes(self):
+        dados_adotantes = []
         if len(self.__adotante_DAO.get_all()) != 0:
             for adotante in self.__adotante_DAO.get_all():
-                self.__tela_adotante.mostra_adotante(adotante)
+                dados_adotantes.append({
+                    'cpf': adotante.cpf,
+                    'nome': adotante.nome,
+                    'data_nascimento': adotante.data_nascimento,
+                    'endereco': adotante.endereco,
+                })
+            
+            self.__tela_adotante.mostra_adotante(dados_adotantes)
         else:
             self.__tela_adotante.mensagem(
                 "Ainda não há adotantes no sistema.")
--- telas/tela_adotante.py
+++ telas/tela_adotante.py
@@ -94,8 +94,13 @@ class TelaAdotante:
         self.close()
         return values['cpf']
 
-    def mostra_adotante(self):
-        ...
+    def mostra_adotante(self, dados_adotantes: list):
+        string_todos_adotantes = ""
+        for adotante in dados_adotantes:
+            string_todos_adotantes += f"{adotante['cpf']} - {adotante['nome']}, {adotante['data_nascimento'].strftime('%d/%m/%Y')}\n"
+            string_todos_adotantes += f"\tEndereço: {adotante['endereco']}\n\n"
+
+        sg.Popup("Lista de adotantes", string_todos_adotantes)
 
     def mensagem(self, mensagem: str):
         sg.Popup("", mensagem)
